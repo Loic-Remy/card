9ed0c40f37159bcd2d178f445b2ca6392ef32c53
Loic-Remy <loic.remy@hotmail.com>
2019-07-06 23:19:11 +0200

    CommencÃ©un fichier "layout.php" pour la mise en page

--- layout.php
+++ layout.php
@@ -2,13 +2,48 @@
 
 class Layout
 {
-	$lineLenth;
-	$lStyle_1;
-	$lStyle_2;
-	$lStyle_3;
+	private $width;
+	private $height;
+	private $lStyle_1;
+	private $lStyle_2;
+	private $lStyle_3;
+
+	public function __construct($width, $height)
+	{
+		$this->width = $width;
+		$this->height = $height;
+	}
+
+	public function PrintLine($char)
+	{
+		for ($i = 0; $i <= $this->width; $i++)
+		{
+			print($char);
+		}
+	}
+
+	public function CenterString($str)
+	{
+		$len = strlen($str);
+		$start = round(($this->width/2)-($len/2));
+
+
+		for ($i = 0; $i <= $start; $i++)
+		{
+			print(" ");
+		}
+		print($str);
+	}
 
 }
 
+
+$page = new Layout(80, 45);
+$page->PrintLine("-");
+print("\n");
+$page->CenterString(" BONJOUR ");
+print("\n");
+$page->PrintLine("*");
 
 
 ?>
